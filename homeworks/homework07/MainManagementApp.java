20a57b4b8550c05df62df4e7e196cf61661a3407
Aleksandr-Burakov <aleks.bur.git@gmail.com>
2023-09-25 02:07:02 +0300

    homework07

--- homeworks/homework07/MainManagementApp.java
+++ homeworks/homework07/MainManagementApp.java
@@ -1,15 +1,24 @@
 package homeworks.homework07;
 import java.util.Scanner;
-
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
   /**
   * Главный класс для демонстрации работы с элементами.
   */
 public class MainManagementApp {
+
+    private static final Logger logger = Logger.getLogger(MainManagementApp.class.getName());
+
   /**
   * Создание экземпляров модели, представления и презентера
   * @controller.calculate Обработка введенных значений с консоли пользователем.
   */
-  public static void main(String[] args) {
+    public static void main(String[] args){
+        Handler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.INFO);
+        logger.info("Info");
         CalculatorModel model = new CalculatorModel();
         CalculatorView view = new CalculatorView();
         CalculatorControllerPresenter controller = new CalculatorControllerPresenter(model, view);
@@ -19,5 +28,6 @@ public class MainManagementApp {
         char operator = (char) view.getUserInput2();   
         controller.calculate(num1, num2, operator);
         scanner.close();
+        
     }
 }
